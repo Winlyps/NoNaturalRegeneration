f8ef8bdc433796679ca7b4a0b18e7b108ae4cc4a
Winlypse <nts3@o2.pl>
2024-10-22 22:38:13 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noNaturalRegeneration/NoNaturalRegeneration.kt
@@ -0,0 +1,14 @@
+package winlyps.noNaturalRegeneration
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoNaturalRegeneration : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
