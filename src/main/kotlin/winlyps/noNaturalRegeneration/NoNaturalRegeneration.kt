3a0de9df13cc0612ddd0f8c392ff830a42412691
Winlypse <nts3@o2.pl>
2024-10-22 22:38:25 +0200

    Changes!

--- src/main/kotlin/winlyps/noNaturalRegeneration/NoNaturalRegeneration.kt
+++ src/main/kotlin/winlyps/noNaturalRegeneration/NoNaturalRegeneration.kt
@@ -6,9 +6,14 @@ class NoNaturalRegeneration : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("naturalRegeneration", "false")
+        }
+        logger.info("NoNaturalRegeneration plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoNaturalRegeneration plugin has been disabled.")
     }
 }
